3b7e28fd3d534451779088cf72e5257768087def
Rijan Bhattarai <rijanbhattarai2006@gmail.com>
2026-07-05 22:26:34 +0545

    System Monitor Inital Phase

---
+++ SYSDOM.c
@@ -0,0 +1,17 @@
+//
+// Created by axelkyu on 7/5/26.
+//
+#include <stdio.h>
+
+struct Monitor {
+    int cpu_temp;
+    float cpu_usage;
+    int gpu_temp;
+    float gpu_usage;
+};
+
+int main(void) {
+    FILE* fp;
+
+    return 0;
+}
