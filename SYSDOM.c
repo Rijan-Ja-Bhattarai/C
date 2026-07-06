d719c94a323b0d26de5e769c5672a24c4c5bda89
Rijan Bhattarai <rijanbhattarai2006@gmail.com>
2026-07-06 09:16:15 +0545

    CPU TEMPRATURE WORKS

--- SYSDOM.c
+++ SYSDOM.c
@@ -8,10 +8,20 @@ struct Monitor {
     float cpu_usage;
     int gpu_temp;
     float gpu_usage;
-};
+} System_Info;
 
 int main(void) {
-    FILE* fp;
+    struct Monitor sys_info;
+    int temp;
+    FILE *g_temp, *g_usage;
+    FILE *c_temp = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+    FILE *c_usage = fopen("proc/stat/ | head", "r");
+    fscanf(c_temp, "%d", &temp);
+    fscanf(c_usage, "%s", &usage);
 
+    printf("%d degree celsius", temp / 1000);
+    printf("%s usage", usage);
+    fclose(c_temp);
+    fclose(c_usage);
     return 0;
 }
